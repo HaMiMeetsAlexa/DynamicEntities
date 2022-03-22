6a2678686fa39ca5a0b09e182e283d2ed7796e59
Hans Ludwig Millmann <hans@millmann.biz>
2022-03-22 13:06:45 +0100

    Added ClearDynamicEntitiesTests.

---
+++ DynamicEntities.Test/ClearDynamicEntitiesTests.cs
@@ -0,0 +1,30 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ClearDynamicEntitiesTests.cs" company="Millmann.Biz">
+//   (c) 2022 by Hans Ludwig Millmann
+// </copyright>
+// <summary>
+//   Defines the ClearDynamicEntitiesTests type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DynamicEntities.Test
+{
+    using Xunit;
+
+    /// <summary>
+    /// The clear dynamic entities tests.
+    /// </summary>
+    public class ClearDynamicEntitiesTests
+    {
+        /// <summary>
+        /// The test clear dynamic entities.
+        /// </summary>
+        [Fact]
+        public void TestClearDynamicEntities()
+        {
+            var directive = ClearDynamicEntities.Clear();
+
+            Assert.NotNull(directive);
+        }
+    }
+}
